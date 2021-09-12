b4ef335057f68ce22c78d3169dd4d1354a0d985f
Eduard Mikalauskas <eduard.mikalauskas@gmail.com>
2021-09-12 16:13:18 +0300

    nio completed

--- io/src/main/kotlin/ru/sber/nio/Grep.kt
+++ io/src/main/kotlin/ru/sber/nio/Grep.kt
@@ -1,5 +1,10 @@
 package ru.sber.nio
 
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
+import kotlin.io.path.isDirectory
+
 /**
  * Реализовать простой аналог утилиты grep с использованием калссов из пакета java.nio.
  */
@@ -15,6 +20,28 @@ class Grep {
      * 22-01-2001-1.log : 3 : 192.168.1.1 - - [22/Jan/2001:14:27:46 +0000] "POST /files HTTP/1.1" 200 - "-"
      */
     fun find(subString: String) {
+        val path = "io/logs"
+        val resultFile = "io/result.txt"
 
+        if (!File(path).exists())
+            return
+
+        if (File(resultFile).exists())
+            File(resultFile).delete()
+
+        Files.walk(Paths.get(path)).forEach { file ->
+            if (file.toString().endsWith(".log")) {
+                val lines = File(file.toUri()).readLines()
+                lines.forEach { line ->
+                    if (line.contains(subString))
+                        File(resultFile).appendText("${file.fileName} : ${lines.indexOf(line) + 1} : $line\n")
+                }
+            }
+        }
     }
+}
+
+fun main() {
+    val grep = Grep()
+    grep.find("[23/Jan/2001:13:00:00 +0000]")
 }
